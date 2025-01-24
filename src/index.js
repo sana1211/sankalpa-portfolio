5fd5b897f0c91b40931884359325e1fbf0fc2eba
sana1211 <sankalpasugathsiri64@gmail.com>
2025-01-24 20:03:59 +0530

    update all

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ThemeContextProvider from './contexts/ThemeContext'
+import ThemeContextProvider from './contexts/ThemeContext';
 
-
-import "slick-carousel/slick/slick.css"; 
+import { HashRouter as Router } from 'react-router-dom'; // Import HashRouter
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
+  <React.StrictMode>
     <ThemeContextProvider>
-      <App />
-    </ThemeContextProvider>,
+      <Router> {/* Wrap your App with Router */}
+        <App />
+      </Router>
+    </ThemeContextProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
 
 reportWebVitals();
