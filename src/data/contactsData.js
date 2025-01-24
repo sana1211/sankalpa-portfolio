f68eda3eca7f19af1c0c6c36db5e60d664b09a71
sana1211 <sankalpasugathsiri64@gmail.com>
2025-01-24 20:27:30 +0530

    Updates

--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-    email: 'sankalpasithmina18@gmail.com',
-    phone: '0702861023 | 0776444215',
-    address: '569/1/3, Kandy Road, Ghanikkulama, Anurradhapura',
-
-    sheetAPI: ''
-}
--- src/data/headerData.js
+++
@@ -1,8 +0,0 @@
-import profile from '../assets/png/my photos.png'
-
-export const headerData = {
-    name: 'Sankalpa Sithmina',
-    title: "Web Developer | UI/UX Designer",
-    desciption:"I am a BIT undergraduate student at the University of Moratuwa, with a strong background in Frontend Development, UI/UX Design and Robotics. With a passion for creating intuitive digital experiences and building innovative systems, I combine creativity and technical expertise to deliver impactful solutions.",
-    image: profile
-}
