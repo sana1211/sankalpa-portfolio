691c9438372b020e337622c9b09e28a0b2449347
sana1211 <sankalpasugathsiri64@gmail.com>
2025-01-27 20:43:53 +0530

    update all

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -4,7 +4,6 @@ import web from '../assets/svg/Achievement/python.jpg'
 import computer from '../assets/svg/Achievement/web.jpg'
 
 export const achievementData = {
-    bio : "The illiterate of the 21st century will not be those who cannot read and write, but those who cannot learn, unlearn, and relearn. - Alvin Toffler",
     achievements : [
         {
             id : 1,
