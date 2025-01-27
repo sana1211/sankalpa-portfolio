6a2c7d83b2d4d10ad6ceafb9fb40a69ed484c8b5
sana1211 <sankalpasugathsiri64@gmail.com>
2025-01-27 15:54:57 +0530

    Updates

--- src/data/aboutData.js
+++
@@ -1,6 +0,0 @@
-export const aboutData = {
-    title: "Who I am",
-    description1: "Hello! My name is W.M.Sankalpa Sithmina Sugathsiri",
-    description2: "BIT undergraduate student from the University of Moratuwa | Specializing in Frontend Development, UI/UX Design | Robotics. As a digital interaction designer, I love to transform ideas into beautifully crafted user experiences that meet the needs of their target audience.",
-    image: 2
-}
